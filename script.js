5075375245c22132f024453036f1a80b60b5e466
Reiggnar <bruno.remy.sebastien@gmail.com>
2023-04-28 12:05:56 +0200

    start-page

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
 // ********** nav toggle ************
 // select button and links
-const navBtn = document.querySelector('.nav-toggle');
-const links = document.querySelector('.nav-links');
-const header = document.querySelector('.header');
+const navBtn = document.querySelector(".nav-toggle");
+const links = document.querySelector(".nav-links");
+const header = document.querySelector(".header");
 
 // add event listener
-navBtn.addEventListener('click', () => {
-  links.classList.toggle('show-links');
+navBtn.addEventListener("click", () => {
+	links.classList.toggle("show-links");
 });
 // ********** smooth scroll ************
 // select links
-const scrollLinks = document.querySelectorAll('.nav-link');
+const scrollLinks = document.querySelectorAll(".nav-link");
 scrollLinks.forEach((link) => {
-  link.addEventListener('click', (e) => {
-    // prevent default
-    e.preventDefault();
-    links.classList.remove('show-links');
+	link.addEventListener("click", (e) => {
+		// prevent default
+		e.preventDefault();
+		links.classList.remove("show-links");
 
-    const id = e.target.getAttribute('href').slice(1);
-    const element = document.getElementById(id);
-    const headerHeight = header.getBoundingClientRect().height;
+		const id = e.target.getAttribute("href").slice(1);
+		const element = document.getElementById(id);
+		const headerHeight = header.getBoundingClientRect().height;
 
-    let position = element.offsetTop - headerHeight;
+		let position = element.offsetTop - headerHeight;
 
-    window.scrollTo({
-      left: 0,
-      top: position,
-      behavior: 'smooth'
-    });
-  });
+		window.scrollTo({
+			left: 0,
+			top: position,
+			behavior: "smooth",
+		});
+	});
 });
 
 //* volume audio
-const audio = document.getElementById('song');
+const audio = document.getElementById("song");
 audio.volume = 0.05;
+
+//  bouton sart
+// select button and links
+const startBtn = document.querySelector(".start-btn");
+const start = document.querySelector(".start");
+
+// add event listener
+startBtn.addEventListener("click", () => {
+	start.classList.toggle("hidden-start");
+});
